3e463a4a00d2cb2b68c34fe0ff8b3cb5071d4a82
ntthanh1976 <49704522+ntthanh1976@users.noreply.github.com>
2023-12-14 14:05:55 +0700

    code version 1

---
+++ MinhHoa_01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhHoa_01
+{
+    struct Diem {
+      public int x;
+      public int y;
+    }
+
+    class Program
+    {
+        static void nhap(ref Diem p)
+        {
+            Console.Write("Nhap hoanh do x:");
+            p.x = int.Parse(Console.ReadLine());
+            Console.Write("Nhap tung do y:");
+            p.y = int.Parse(Console.ReadLine());
+        }
+
+        static void xuat(Diem p, string name)
+        {
+            Console.WriteLine("{0}({1},{2})", name, p.x, p.y);
+        }
+        static void Main(string[] args)
+        {
+            Diem p1= new Diem(), p2= new Diem();
+            nhap(ref p1);
+            nhap(ref p2);
+            xuat(p1,"A");
+            xuat(p2,"B");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MinhHoa_02/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhHoa_02
+{
+    class Diem
+    {
+        private int x;
+        private int y;
+
+        public Diem() {
+            x = 0;
+            y = 0;
+        }
+        public Diem(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void setX(int x)
+        {
+            this.x = x;
+        }
+        public void setY(int y)
+        {
+            this.y = y;
+        }
+
+        public int getX()
+        {
+            return x;
+        }
+        public int getY()
+        {
+            return y;
+        }
+
+        public void xuat(string name)
+        {
+            Console.WriteLine("{0}({1},{2})",name,x,y);
+        }
+      
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diem p1 = new Diem();
+            p1.setX(2);
+            p1.setY(10);
+            p1.xuat("A");
+
+            Diem p2 = new Diem();
+            p2.setX(7);
+            p2.setY(15);
+            p2.xuat("B");
+            Console.ReadLine();
+        }
+    }
+}
